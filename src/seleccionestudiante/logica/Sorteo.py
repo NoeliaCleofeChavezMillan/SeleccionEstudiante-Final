27b5cd76ad2736ca463de31e7cea53a8f7e0bdca
NoeliaCleofeChavezMillan <71261229@continental.edu.pe>
2021-11-29 02:09:22 -0500

    se actualizo el modelo tests y modelo

---
+++ src/seleccionestudiante/logica/Sorteo.py
@@ -0,0 +1,37 @@
+from src.seleccionestudiante.modelo.Asignatura import Asignatura
+from src.seleccionestudiante.modelo.Estudiante import Estudiante
+from src.seleccionestudiante.modelo.declarative_base import engine, Base, session
+
+class Sorteo():
+
+    def __init__(self):
+        Base.metadata.create_all(engine)
+
+    def agregar_asignatura(self, nombreAsignatura):
+        if(nombreAsignatura==""):
+            return False
+
+        busqueda = session.query(Asignatura).filter(Asignatura.nombreAsignatura == nombreAsignatura).all()
+        if len(busqueda) == 0:
+            asignatura = Asignatura(nombreAsignatura=nombreAsignatura)
+            session.add(asignatura)
+            session.commit()
+            return True
+        else:
+            return False
+
+    def agregar_estudiante(self, apellidoPaterno, apellidoMaterno, nombres, elegible):
+        if(apellidoPaterno=="" and apellidoMaterno=="" and nombres=="" and elegible==""):
+            return False
+
+        busqueda2 = session.query(Estudiante).filter(Estudiante.apellidoPaterno == apellidoPaterno,
+                                                     Estudiante.apellidoMaterno == apellidoMaterno,
+                                                     Estudiante.nombres == nombres,
+                                                     Estudiante.elegible == elegible).all()
+        if len(busqueda2) == 0:
+            estudiante = Estudiante(apellidoPaterno=apellidoPaterno,apellidoMaterno=apellidoMaterno,nombres=nombres,elegible=elegible)
+            session.add(estudiante)
+            session.commit()
+            return True
+        else:
+            return False
